1060db5a5fc0ade5ee812024b6eb3afbadcd784d
Alexander Kohlhofer <alexander@kohlhofer.com>
2012-06-01 12:49:12 -0700

    combined the decryption and encryption into one page again

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/encrypt', {template: 'partials/encrypt.html', controller: encryptCtrl});
-    $routeProvider.when('/decrypt', {template: 'partials/decrypt.html', controller: decryptCtrl});
+    $routeProvider.when('/aes', {template: 'partials/aes.html', controller: aesCtrl});
     $routeProvider.when('/phrases', {template: 'partials/phrases.html', controller: phrasesCtrl});
     $routeProvider.when('/about', {template: 'partials/about.html', controller: aboutCtrl});
     $routeProvider.otherwise({redirectTo: '/encrypt'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,15 +3,15 @@
 /* Controllers */
 
 
-function encryptCtrl($scope,WordService) {
+function aesCtrl($scope,WordService) {
 
   var passPhraseWordCount = 18;
 
   $scope.message = '';
   $scope.cypher = '';
   $scope.cypherString = '';
-  $scope.passphrase = WordService.getWords(passPhraseWordCount);
-  $scope.status = '';
+  $scope.passphrase = '';
+  $scope.feedBack = '';
 
 
 
@@ -25,15 +25,46 @@ function encryptCtrl($scope,WordService) {
 
       $scope.message = '';
 
-      $scope.status = 'You Message was successfully encrypted. See Cypher and Pass Phrase below.';
+      $scope.feedBack = 'You Message was successfully encrypted.';
 
     } else {
 
-      $scope.status = 'Message and Pass phrase are required';
+      $scope.feedBack = 'Message and Pass phrase are required';
 
     }
     
   }
+
+
+  $scope.decrypt = function () {
+
+    
+    if ($scope.cypher == '' | $scope.passphrase == '' ) {
+
+      $scope.feedBack = 'Cypher and Pass phrase are required';
+
+    } else {
+
+      $scope.message = CryptoJS.AES.decrypt($scope.cypher, $scope.passphrase).toString(CryptoJS.enc.Utf8);
+
+      if ($scope.message == '') {
+
+        $scope.feedBack = 'Cypher and Key do not match';
+
+      } else {
+
+        $scope.cypherString = '';
+
+        $scope.cypher = '';
+
+        $scope.feedBack = 'Your Cypher was successfully decrypted.';
+
+      }
+
+    }
+
+  }
+
 
   $scope.newPassPhrase = function () {
 
@@ -47,67 +78,13 @@ function encryptCtrl($scope,WordService) {
     $scope.cypher = '';
     $scope.cypherString = '';
     $scope.passphrase = '';
-    $scope.status = '';
+    $scope.feedBack = '';
     
   }
 
 }
 
 
-
-
-
-
-function decryptCtrl($scope) {
-
-  $scope.message = '';
-  $scope.cypher = '';
-  $scope.cypherString = '';
-  $scope.passphrase = '';
-  $scope.status = '';
-
-  $scope.decrypt = function () {
-
-    
-    if ($scope.cypher == '' | $scope.passphrase == '' ) {
-
-      $scope.status = 'Cypher and Pass phrase are required';
-
-    } else {
-
-      $scope.message = CryptoJS.AES.decrypt($scope.cypher, $scope.passphrase).toString(CryptoJS.enc.Utf8);
-
-      if ($scope.message == '') {
-
-        $scope.status = 'Cypher and Key do not match';
-
-      } else {
-
-        $scope.cypherString = '';
-
-        $scope.cypher = '';
-
-        $scope.passphrase = '';
-
-        $scope.status = 'Your Cypher was successfully decrypted. See message below.';
-
-      }
-
-    }
-
-  }
-
-  $scope.clear = function () {
-
-    $scope.message = '';
-    $scope.cypher = '';
-    $scope.cypherString = '';
-    $scope.passphrase = '';
-    $scope.status = '';
-    
-  }
-
-}
 
 
 
